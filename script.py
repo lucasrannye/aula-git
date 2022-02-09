172c3a80fec35469ac91adfb0a9064cd6c9a82e7
Lucas Rannye <lucas.rannye@hotmail.com>
2022-02-09 12:45:46 -0300

    Inicio do manual

--- script.py
+++ script.py
@@ -1,3 +1,22 @@
-#Imports
+#Aula de GIT
 
-#Código novo
+#Comandos
+## git init - inicializa o git em determinada pasta/local
+## git status - verifica os arquivos da pasta
+## git add "nome do arquivo.txt" - adiciona o arquivo no git
+## git add . - adiciona todos os arquivos da pasta no git
+## git commit -m "commit inicial" - adiciona uma nova versão do projeto * uma boa prática é commitar inicial
+## git remote origin *link do repositório no github* - adiciona o link do repositório que você quer
+## git push - sobe o projeto para o github
+## git reflog - mostra as versões do projeto // a versão mais atual é sempre a do topo
+## git reset --hard *id da versão* - retorna uma versão do projeto
+## git branch - mostra quais branchs estão disponíveis
+## git branch *nome da branch* - adiciona uma nova branch
+## git checkout *nome da branch* - altera para a branch que você quer
+##
+
+#Branchs
+## São ramificações do projeto durante o processo de desenvolvimento
+
+#Merge
+## É a junção de uma branch paralela para a branch principal
